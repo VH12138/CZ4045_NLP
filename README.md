05fae1aa11e79e6c60bd187f3384b7f3e0351e2b
ConyYang <yangyubei0218@gmail.com>
2021-10-24 15:39:08 +0800

    Readme update

--- README.md
+++ README.md
@@ -35,30 +35,41 @@ pip install <package_name>
 ## Program Execution and Desired Outcome
 
 ### 3.2 Dataset Analysis
-* Change directory accordingly so that the dataeset can be accessed:
+* Make a new directory name 'Dataset' under the root directory. 
+Put the file 'reviewSelected100.json' under this 'Dataset' directory.
 ```
-json_path = '../Dataset/reviewSelected100.json' 
+sudo mkdir Dataset
 ``` 
+* Switch to directory Task3.2 to run the following 3 python files.
+```
+cd Task3.2
+```
 * Tokenization and Stemming: Run the following command under folder Task3.2
 ```
 python tokenization_stemming.py
 ```
-
 * A result folder will be generated. Four files will be stored inside the result folder:
     * b1review.txt: Extracted all reviews for random selected business b1
     * b2review.txt: Extracted all reviews for random selected business b2
     * b1_word_frequency.png: Word frequency for b1
     * b2_word_frequency.png: Word frequency for b2
-
-
+  
 * POS Tagging: Run the following command under folder Task3.2
 ```
 python POS_tag.py
 ```
-* Output of the two methods of POS Tagging will be shown in ternimal. 
+* Output of the two methods of POS Tagging will be shown in terminal. <br/>
+
+
+* Most Frequent Noun-Adj-Pairs
+```
+python Noun_Adj_Pair.py
+```
+* Output of this file is saved in result folder as a .csv, with name 'noun_adj_starN.csv'. N refers to the star you want to analysis.
+You can change the star value with 1,2,3,4,5 in the 10th line of file Noun_Adj_Pair.py.
 
 ### 3.3 Extraction of Adjective Phrases (AP)
-* Change directory accordingly so that the dataeset can be accessed:
+* Change directory accordingly so that the dataset can be accessed:
 ```
 json_path = '../Dataset/reviewSelected100.json' 
 ``` 
--- Task3.2/Noun_Adj_Pair.py
+++ Task3.2/Noun_Adj_Pair.py
@@ -7,29 +7,29 @@ import pandas as pd
 import os
 
 if __name__ == '__main__':
+    STAR = '5'  # Change this star parameter if needed
+
     if not os.path.exists('extract_NJ_pairs/processedData'):
         filepath = '../Dataset/reviewSelected100.json'
         os.makedirs('extract_NJ_pairs/processedData')
         save_folder = 'extract_NJ_pairs/processedData'
         generate_json_grpby_bsid_diff_star(filepath, save_folder)
 
-    star = '5' # only parameter need to pass
-
-    if star == '1':
+    if STAR == '1':
         no_review = 50
     else:
         no_review = 20
     if not os.path.exists('extract_NJ_pairs/processedData/pickle_data'):
         os.makedirs('extract_NJ_pairs/processedData/pickle_data')
 
-    path_read = 'extract_NJ_pairs/processedData/text_grpbsid_star'+star+'.json'
-    path_write = 'extract_NJ_pairs/processedData/pickle_data/texts_star'+star +'.p'
+    path_read = 'extract_NJ_pairs/processedData/text_grpbsid_star' + STAR + '.json'
+    path_write = 'extract_NJ_pairs/processedData/pickle_data/texts_star' + STAR + '.p'
     myRandReview = RandReview(path_read, path_write, no_review)
     myRandReview.read_text()
     myRandReview.select_rand_reviews()
     myRandReview.write_selected_reviews_pickle()
 
-    texts_star1 = pickle.load(open('extract_NJ_pairs/processedData/pickle_data/texts_star'+star+'.p', 'rb'))
+    texts_star1 = pickle.load(open('extract_NJ_pairs/processedData/pickle_data/texts_star' + STAR + '.p', 'rb'))
     pairs_list = []
     for text in texts_star1:
         myTextPreprocessor = TextPreprocessor(text)
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         pairs = myNounAdjPair.get_pairs_one_text()
         pairs_list += pairs
     df = pd.DataFrame(pairs_list, columns=['Noun', 'Adj'])
-    df.to_csv('result/noun_adj_star'+star+'.csv', index=False)
+    df.to_csv('result/noun_adj_star' + STAR + '.csv', index=False)
 
-    path = 'result/noun_adj_star'+star+'.csv'
+    path = 'result/noun_adj_star' + STAR + '.csv'
     get_top_frequenct_nj_pairs(path)
